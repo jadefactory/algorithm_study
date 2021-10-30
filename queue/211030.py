f3e1a02075c19b04615c5a52caec0ba14e3d824a
jadeplanet <seychelle86@gmail.com>
2021-10-30 18:03:20 +0900

    baekjoon_2161 : 카드1

---
+++ queue/211030.py
@@ -0,0 +1,21 @@
+# Memory 34924KB / Time 120ms
+import queue
+
+N = int(input())
+
+data = queue.Queue()
+
+for i in range(N):
+    data.put(i+1)
+
+thrown_card = []
+while data.qsize() != 1:
+    thrown_card.append(data.get())
+    remain_card = data.get()
+    data.put(remain_card)
+
+last_card = data.get()
+thrown_card.append(last_card)
+
+for i in thrown_card:
+    print(int(i), end=' ')
